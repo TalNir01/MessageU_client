5ae164c16c541aa2d0a77030051c07817b37500e
Tal Nir <taliknir@gmail.com>
2021-10-22 13:01:56 +0300

    21 October 13:01

--- SocketClass.cpp
+++ SocketClass.cpp
@@ -2,11 +2,49 @@
 
 SocketClass::SocketClass() 
 {
-	std::cout << "Hello From Socket Class" << std::endl;
-	this->printing_Stuff(5);
+	/*
+		Constructor for SocketClass, creating the connection, Constructor for first time log in.
+	*/
+	std::cout << "[LOG] Hello World From SocketClass Constructor " << std::endl;
+	this->ip = "";
+	this->port = "";
+	this->read_server_info();
+
+	// Init boost connection
+	boost::asio::io_context io_context;
+	tcp::socket client_sock_conn(io_context);
+	tcp::resolver resolver(io_context);
+
+	this->
+	// this->client_sock_conn(this->io_context);
+
+
 }
 
 void SocketClass::printing_Stuff(int i) 
 {
 	std::cout << i << std::endl;
+}
+
+void SocketClass::read_server_info() 
+{
+	
+	FILE *server_info_file;
+	server_info_file = fopen("server.info", "r");
+	if (!server_info_file) {
+		std::cout << "[LOG]Error: server.info opening failed " << std::endl;
+	}
+	else{
+		char tmp;
+		while ((tmp = fgetc(server_info_file)) != ':' and tmp != EOF) 
+		{
+			this->ip += tmp; // add char to ip until reaching ':'
+		}
+		while ((tmp = fgetc(server_info_file)) != EOF) 
+		{
+			this->port += tmp; // add char to port until reaching EOF
+		}
+	}
+
+
 }
--- SocketClass.h
+++ SocketClass.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <iostream>
 #include <boost/asio.hpp>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <cstdio>
+
+
 
 #define MAX_LENGTH 10
 
@@ -12,7 +18,16 @@ public:
 	SocketClass();
 
 private:
+	std::string ip;
+	std::string port;
+
+	//boost::asio::io_context io_context;
+	//tcp::socket* client_sock_conn;
+	//tcp::resolver resolver;
+
 	void printing_Stuff(int i);
+
+	void read_server_info();
 	
 };
 
